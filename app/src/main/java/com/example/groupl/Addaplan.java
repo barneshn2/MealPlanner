a1e27942d3ec185f8effd59057af6f28da1089f7
Karim <mahmoudkn@mymail.vcu.edu>
2020-10-01 23:21:34 -0400

    Made a "Back" button that takes u to the previous page

--- app/src/main/java/com/example/groupl/Addaplan.java
+++ app/src/main/java/com/example/groupl/Addaplan.java
@@ -56,7 +56,8 @@ public class Addaplan extends AppCompatActivity {
     }
 
 
-
-
-
+    public void backbtn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
