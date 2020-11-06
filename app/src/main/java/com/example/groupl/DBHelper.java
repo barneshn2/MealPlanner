f4d2297f8a165618cb8701fd2fff847a251aebfb
Karim <mahmoudkn@mymail.vcu.edu>
2020-11-05 21:44:45 -0500

    commented the database functions.

--- app/src/main/java/com/example/groupl/DBHelper.java
+++ app/src/main/java/com/example/groupl/DBHelper.java
@@ -7,10 +7,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
     public static String TableName="RacpiTable";
     public static String TableName2="IngriendensTable";
-    public static String C1="id";
-    public static String C2="name";
-    public static String C3="r_id";
-    public static String C4="date";
+    public static String C1="id";   //Primary key
+    public static String C2="name"; //Recipe and ingredient name
+    public static String C3="r_id"; //foreign key for recipe
+    public static String C4="date"; //Date
 
     public DBHelper(Context context){
         super(context, TableName,null,3);
@@ -21,11 +21,11 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
 
-        String Sql = "CREATE TABLE IF NOT EXISTS "
+        String Sql = "CREATE TABLE IF NOT EXISTS " //recipes table
                 + TableName + "(" + C1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + C2 + " text ,"+C4+" INTEGER)";
 
-        String Sql2 = "CREATE TABLE IF NOT EXISTS "
+        String Sql2 = "CREATE TABLE IF NOT EXISTS " //ingredients table
                 + TableName2 + "(" + C1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + C2 + " text ,"+ C3 +" INTEGER )";
 
--- app/src/main/java/com/example/groupl/DBManger.java
+++ app/src/main/java/com/example/groupl/DBManger.java
@@ -18,7 +18,7 @@ public class DBManger {
 
 
     }
-    public void insertRecpiToDataBase(Recipe r) {
+    public void insertRecpiToDataBase(Recipe r) { //this function will automatically insert Recipes to database
         ContentValues values = new ContentValues();
         values.put(DBHelper.C2, r.getName());
         values.put(DBHelper.C4, r.getLastUpdate());
@@ -30,7 +30,7 @@ public class DBManger {
             }
         }
     }
-    public int getLastId(){
+    public int getLastId(){ //this function will organize the order of the recipes in the database. forexample, the last entered recipe will be all the way the bottom of the table
         String SQl = "Select * From " + DBHelper.TableName+" ORDER BY "+ DBHelper.C1 +" DESC LIMIT 1";
         Cursor cursor = db.rawQuery(SQl, null);
         int id=0;
@@ -44,7 +44,7 @@ public class DBManger {
         }
 
 
-    public boolean insertIngToDataBase(Ingredients r) {
+    public boolean insertIngToDataBase(Ingredients r) { //checks the validity of the database
 
         ContentValues values = new ContentValues();
         values.put(DBHelper.C2, r.getContent());
@@ -53,7 +53,7 @@ public class DBManger {
     }
 
 
-    public List<Recipe> getAllRecipe() {
+    public List<Recipe> getAllRecipe() { //this function will send all the recipes in the database to an arraylist to be printed in the recipe layout
         List<Recipe> ret = new ArrayList<>();
         String SQl = "Select * From " + DBHelper.TableName;
         Cursor cursor = db.rawQuery(SQl, null);
@@ -72,7 +72,7 @@ public class DBManger {
         cursor.close();
         return ret;
     }
-    public Recipe getOneRecipeByID(int id ) {
+    public Recipe getOneRecipeByID(int id ) {//allows you to view each recipe by using the ID
         Recipe r=new Recipe();
         String SQl = "Select * From " + DBHelper.TableName+" where "+ DBHelper.C1+"="+id;
         Cursor cursor = db.rawQuery(SQl, null);
@@ -91,7 +91,7 @@ public class DBManger {
     }
 
 
-    public List<Ingredients> getAllIngredients(int id) {
+    public List<Ingredients> getAllIngredients(int id) { //this function will send all the ingredients for each recipe to an arrayslist, so it shows all the ingreidents for each recipe
         List<Ingredients> ret = new ArrayList<>();
         String SQl = "Select * From " + DBHelper.TableName2 + " Where " + DBHelper.C3 + "=" + id;
         Cursor cursor = db.rawQuery(SQl, null);
@@ -111,7 +111,7 @@ public class DBManger {
     }
 
 
-    public void updateRecpiToDataBase(Recipe r) {
+    public void updateRecpiToDataBase(Recipe r) {//this functions will replace ingredients in databse for new chosen ingreidents by the user
         ContentValues values = new ContentValues();
         values.put(DBHelper.C2, r.getName());
         values.put(DBHelper.C4, r.getLastUpdate());
@@ -124,12 +124,12 @@ public class DBManger {
         }
     }
 
-    private void deleteIngrad(int id ){
+    private void deleteIngrad(int id ){ //this function deletes ingredients from databse
           boolean b=db.delete(DBHelper.TableName2, DBHelper.C3 + "=" + id, null) > 0;
 
     }
 
-    public void deleteRecipe(int id){
+    public void deleteRecipe(int id){//this function deletes recipes from database
         db.delete(DBHelper.TableName,DBHelper.C1+"="+id,null);
         db.delete(DBHelper.TableName2,DBHelper.C3+"="+id,null);
 
