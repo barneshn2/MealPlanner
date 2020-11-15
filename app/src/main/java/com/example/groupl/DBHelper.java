9a1c7a7865d5663703b617519487ac7846387ada
Karim <mahmoudkn@mymail.vcu.edu>
2020-11-15 15:45:25 -0500

    Created 2 new Database tables.

--- app/src/main/java/com/example/groupl/DBHelper.java
+++ app/src/main/java/com/example/groupl/DBHelper.java
@@ -7,10 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
     public static String TableName="RacpiTable";
     public static String TableName2="IngriendensTable";
+    public static String TableName3="PlansTable";
+    public static String TableName4="RecipeAndPlanRelation";
     public static String C1="id";   //Primary key
     public static String C2="name"; //Recipe and ingredient name
     public static String C3="r_id"; //foreign key for recipe
     public static String C4="date"; //Date
+    public static String C5="Plan_id"; //Date
+    public static String C6="plan_name"; //Date
 
     public DBHelper(Context context){
         super(context, TableName,null,3);
@@ -29,6 +33,13 @@ public class DBHelper extends SQLiteOpenHelper {
                 + TableName2 + "(" + C1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + C2 + " text ,"+ C3 +" INTEGER )";
 
+        String Sql3 = "CREATE TABLE IF NOT EXISTS " //ingredients table
+                + TableName3 + "(" + C1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + C6 + " text)";
+
+        String Sql4 = "CREATE TABLE IF NOT EXISTS " //ingredients table
+                + TableName4 + "(" + C1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + C5 + " INTEGER ,"+C3+" INTEGER )";
 
         db.execSQL(Sql2);
         db.execSQL(Sql);
