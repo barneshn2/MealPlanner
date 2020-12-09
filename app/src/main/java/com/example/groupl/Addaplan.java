e0d6d755196f4e22eb77a24db556397f3b84df95
Karim <mahmoudkn@mymail.vcu.edu>
2020-12-08 23:45:04 -0500

    add a plan is finished

--- app/src/main/java/com/example/groupl/Addaplan.java
+++ app/src/main/java/com/example/groupl/Addaplan.java
@@ -8,38 +8,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Addaplan extends AppCompatActivity {
     private List<Recipe> allrecipes = new ArrayList<>();
     private List<Recipe> selectedrecipesList = new ArrayList<>();
+    private DBManger Db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_addaplan);
         setContentView(R.layout.addplan);
         Intent intent = getIntent();
-        Button saver = findViewById(R.id.button3);
+        getSupportActionBar().hide();
 
-        DBManger Db = new DBManger(Addaplan.this);
+        Db = new DBManger(Addaplan.this);
         RecyclerView AllRecipes = findViewById(R.id.SHOW_ALL);
-        final AllRecipeAdapter adapter=new AllRecipeAdapter();
+        AllRecipewithselectionAdapter adapter=new AllRecipewithselectionAdapter();
         AllRecipes.setLayoutManager(new LinearLayoutManager(this));
         AllRecipes.setAdapter(adapter);
         allrecipes = Db.getAllRecipe();
         adapter.setDataList(allrecipes);
-        getSupportActionBar().hide();
-        saver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
     public void backbtn(View view) {
         Intent intent = new Intent(this, MainActivity.class);
@@ -51,20 +44,29 @@ public class Addaplan extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void save(){
-            for (Recipe selectedRecipe:allrecipes){
-                if (selectedRecipe.isChecked()){
-                    if (!selectedrecipesList.contains(selectedRecipe)){
-                        selectedrecipesList.add(selectedRecipe);
-                        Log.e("selectPlan ",selectedRecipe.getId()+" ");
-                    }
-                }else {
-                    if (selectedrecipesList.contains(selectedRecipe)){
-                        selectedrecipesList.remove(selectedRecipe);
-                    }
+    public void Save(View view) {
+        for (Recipe selectedRecipe:allrecipes){
+            if (selectedRecipe.isChecked()){
+                if (!selectedrecipesList.contains(selectedRecipe)){
+                    selectedrecipesList.add(selectedRecipe);
+                    Log.e("selectPlan ",selectedRecipe.getId()+" ");
                 }
-
+            }else {
+                if (selectedrecipesList.contains(selectedRecipe)){
+                    selectedrecipesList.remove(selectedRecipe);
+                }
             }
-            Toast.makeText(this, "SelectedItemsSize = "+selectedrecipesList.size(), Toast.LENGTH_SHORT).show();
+
+        }
+        Toast.makeText(this, "SelectedItemsSize = "+selectedrecipesList.size(), Toast.LENGTH_SHORT).show();
+        Plan newPlan=new Plan();
+        newPlan.setPlanName("My New Plan");
+        newPlan.setRecipeList(selectedrecipesList);
+        Calendar c=Calendar.getInstance();
+        c.set(Calendar.DAY_OF_MONTH,c.get(Calendar.DAY_OF_MONTH)+1);
+        newPlan.setDate(c.getTimeInMillis());
+        Db.inserNewPlan(newPlan);
+        finish();
     }
+
 }
